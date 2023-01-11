b5b04109e05fdbdb12d025501bea5f2efbe030bd
John Bjorge <johnedwardbjorge@gmail.com>
2023-01-11 00:56:02 -0800

    weekly, quarterly, yearly award queries

--- cogs/cron_jobs.py
+++ cogs/cron_jobs.py
@@ -24,26 +24,29 @@ class CronJobs(commands.Cog):
 
             sql_input = {"current_date": current_date}
 
-            sql_insert_fitness_goal = \
+            sql_date_dimension_current_date = \
                 ("""
                     select day_of_week,
                     day_of_quarter,
-                    day_of_year
+                    day_of_year,
+                    week_of_year,
+                    quarter_actual,
+                    year_actual
                     from date_dimension 
                     where date_actual = %(current_date)s;
                 """)
 
-            query, positional_args = db_manager.pyformat_to_psql(sql_insert_fitness_goal, sql_input)
+            query, positional_args = db_manager.pyformat_to_psql(sql_date_dimension_current_date, sql_input)
 
             logger.debug(f'Returning date information for current date: {current_date}\n')
 
             result = await self.bot.db.fetchrow(query, *positional_args)
 
             if result["day_of_week"] == 7:
-                await awards.award_weekly(self)
+                await awards.award_weekly(self, result["year_actual"], result["week_of_year"])
 
             if result["day_of_quarter"] == 1:
-                await awards.award_quarterly(self)
+                await awards.award_quarterly(self, result["year_actual"], result["quarter_actual"])
 
             if result["day_of_year"] == 1:
                 await awards.award_yearly(self)
--- helpers/awards.py
+++ helpers/awards.py
@@ -1,28 +1,114 @@
 import logging
+from helpers import db_manager
 
 
 logger = logging.getLogger(__name__)
 
 
-# todo: see query notes for this stuff, I think it makes sense to make an award table
-#  essentially a scoreboard for all awards, just need to filter on year/week or year/quarter or year
-#  need to be careful about how week_of_year rolls over years/quarters and how that could impact counts
-async def award_weekly(self):
-    # todo: put embed here
-    #  weekly shoutouts on Sunday (Query #1)
-    #         list all users that have more than half of days over the last week with some activity
-    #             consider ordering them by sum(difficulty) for a twinge of competitiveness
-    await self.bot.get_channel(1045906913080115225).send("This is a weekly goal trigger")
+def award_workouts_scoreboard():
+    sql_workouts_scoreboard = \
+    ("""
+        with workouts_scoreboard as (
+            select w.user_id,
+            dd.week_of_year,
+            dd.quarter_actual,
+            dd.year_actual,
+            count(*) as total_workouts,
+            sum(difficulty) as total_difficulty,
+            case when count( * ) >= 4 then 1 else 0 end as consistency_flag
+            from workout w
+            inner join date_dimension dd
+            on w.date = dd.date_actual
+            group by w.user_id, dd.week_of_year, dd.quarter_actual, dd.year_actual
+        )\n\n
+    """)
+
+    return sql_workouts_scoreboard
 
 
-async def award_quarterly(self):
-    # todo: put embed here
-    #  weekly badge - consistency (need name for award), if 4+ days activity every week in given quarter
-    await self.bot.get_channel(1045906913080115225).send("This is a quarterly goal trigger")
+# todo: has not been tested
+async def award_weekly(self, ctx, year_actual, week_of_year):
+    sql_cte_workouts_scoreboard = award_workouts_scoreboard()
+
+    sql_input = {"year_actual": year_actual, "week_of_year": week_of_year}
+
+    sql_award_weekly = \
+        ("""
+            select user_id,
+            year_actual,
+            week_of_year,
+            sum(total_workouts),
+            round(sum(total_difficulty) / sum(total_workouts), 2) as avg_difficulty
+            from workouts_scoreboard
+            where year_actual = %(year_actual)s
+            and week_of_year = %(week_of_year)s
+            and consistency_flag = 1
+            group by user_id, year_actual, week_of_year;
+        """)
+
+    query, positional_args = db_manager.pyformat_to_psql(sql_cte_workouts_scoreboard + sql_award_weekly, sql_input)
+
+    logger.debug(f'Returning award list for year {year_actual} and week {week_of_year}\n')
+
+    result = await self.bot.db.fetch(query, *positional_args)
+
+    # todo: turn this into an embed
+    await self.bot.get_channel(1045906913080115225).send(result)
 
 
-async def award_yearly(self):
-    # todo: put embed here
-    #  yearly badge - consistency (need name for award), if all 4 quarters in given year "consistent"
-    await self.bot.get_channel(1045906913080115225).send("This is a yearly goal trigger")
+# todo: has not been tested
+async def award_quarterly(self, year_actual, quarter_actual):
+    sql_cte_workouts_scoreboard = award_workouts_scoreboard()
 
+    sql_input = {"year_actual": year_actual, "quarter_actual": quarter_actual}
+
+    sql_award_quarterly = \
+        ("""
+            select user_id, 
+            year_actual, 
+            quarter_actual, 
+            sum(total_workouts) as total_workouts,
+            round(sum(total_difficulty) / sum(total_workouts), 2) as avg_difficulty,
+            case when min(consistency_flag) = 0 then 0 else 1 end as consistency_flag
+            from workouts_scoreboard
+            where year_actual = %(year_actual)s
+            and quarter_actual = %(quarter_actual)s - 1
+            group by user_id, year_actual, quarter_actual;
+        """)
+
+    query, positional_args = db_manager.pyformat_to_psql(sql_cte_workouts_scoreboard + sql_award_quarterly, sql_input)
+
+    logger.debug(f'Returning award list for year {year_actual} and quarter {quarter_actual}\n')
+
+    result = await self.bot.db.fetch(query, *positional_args)
+
+    # todo: turn this into an embed
+    await self.bot.get_channel(1045906913080115225).send(result)
+
+
+# todo: has not been tested
+async def award_yearly(self, year_actual):
+    sql_cte_workouts_scoreboard = award_workouts_scoreboard()
+
+    sql_input = {"year_actual": year_actual}
+
+    sql_award_yearly = \
+        ("""
+            select user_id, 
+            year_actual, 
+            sum(total_workouts) as total_workouts,
+            round(sum(total_difficulty) / sum(total_workouts), 2) as avg_difficulty,
+            case when min(consistency_flag) = 0 then 0 else 1 end as consistency_flag
+            from workouts_scoreboard
+            where year_actual = %(year_actual)s
+            group by user_id, year_actual;
+        """)
+
+    query, positional_args = db_manager.pyformat_to_psql(sql_cte_workouts_scoreboard + sql_award_yearly, sql_input)
+
+    logger.debug(f'Returning award list for year {year_actual}\n')
+
+    result = await self.bot.db.fetch(query, *positional_args)
+
+    # todo: turn this into an embed
+    await self.bot.get_channel(1045906913080115225).send(result)
